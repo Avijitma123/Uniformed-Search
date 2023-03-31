85aad0fd158f3aed96c778dc185d7fe945bb46dd
Avijit Dey <64518285+Avijitma123@users.noreply.github.com>
2023-03-31 16:23:05 +0530

    Update 15_puzzle_problem.cpp

--- 15_puzzle_problem.cpp
+++ 15_puzzle_problem.cpp
@@ -235,9 +235,9 @@ Board copyBoard(Board board, int n)
 //----------------------------------------------------- 
 //Define a function to check if the state is already in the successor list
 bool inSucc(Board board)
-{
+{    
     //check if the state is already in the successor queue
-    queue<Board> temp = succ;
+    queue<Board> temp(succ);
     while(!temp.empty())
     {
         if(temp.front() == board)
@@ -247,45 +247,182 @@ bool inSucc(Board board)
     return false;
     
 }
-
+//calculate the number of misplaced tiles
+int getMisplaced(Board board, Board f_state)
+{
+    int misplaced = 0;
+    for(int i = 0; i < board.n; i++)
+    {
+        for(int j = 0; j < board.n; j++)
+        {
+            if(board.vec[i][j] != f_state.vec[i][j])
+                misplaced++;
+        }
+    }
+    return misplaced;
+}
+int getDistance(Board board, Board f_state)
+{
+    int distance = 0;
+    for(int i = 0; i < board.n; i++)
+    {
+        for(int j = 0; j < board.n; j++)
+        {
+            if(board.vec[i][j] != 0)
+            {
+                int x = board.vec[i][j];
+                for(int k = 0; k < board.n; k++)
+                {
+                    for(int l = 0; l < board.n; l++)
+                    {
+                        if(f_state.vec[k][l] == x)
+                        {
+                            distance += abs(i-k) + abs(j-l);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return distance;
+}
 //-----------------------------------------------------
 //Define a function to generate the successors
-void generateSuccessors(Board board,int n)
+int generateSuccessors(Board board,int n, Board f_state,int current)
 {
-   
+    
     int x = board.blankX;
     int y = board.blankY;
     Board temp1 = copyBoard(board,n);
     Board temp2 = copyBoard(board,n);
     Board temp3 = copyBoard(board,n);
     Board temp4 = copyBoard(board,n);
-    temp1.moveUp(x, y);
-    if(!checkVisited(temp1) && !inSucc(temp1) )
+
+    int r = rand()%100;
+
+    if(r%2==0){
+          temp1.moveUp(x, y);
+
+    if(!checkVisited(temp1) && !inSucc(temp1))
     {
-        succ.push(temp1);
         
+            
+            succ.push(temp1);
+        
+        
+             
     }
+       
     
     temp2.moveDown(x, y);
     if(!checkVisited(temp2) && !inSucc(temp2))
     {
-        succ.push(temp2);
+        
+        if(getDistance(temp2, f_state) < current)
+        {
+            current = getDistance(temp2, f_state);
+            succ.push(temp2);
+        }
+        
+        
+    }
+     temp3.moveLeft(x, y);
+    if(!checkVisited(temp3)&& !inSucc(temp3) )
+    {
+        
+            
+                
+                succ.push(temp3);
+            
+        
+        
+    }
+    
+    
+    
+    temp4.moveRight(x, y);
+    if(!checkVisited(temp4)&& !inSucc(temp4))
+    {
+        
+            
+                if(getDistance(temp4, f_state) < current)
+                {
+                    current = getDistance(temp4, f_state);
+                    succ.push(temp4);
+                }
+                
+            
+        
+        
+    }
+
+    }
+    else 
+    {
+      temp1.moveUp(x, y);
+
+    if(!checkVisited(temp1) && !inSucc(temp1))
+    {
+        if(getDistance(temp1, f_state) < current)
+        {
+            current = getDistance(temp1, f_state);
+            succ.push(temp1);
+        }
+        
+             
+        
+        
+        
+    } 
+    
+    temp2.moveDown(x, y);
+    if(!checkVisited(temp2) && !inSucc(temp2))
+    {
+        
+        
+            
+            succ.push(temp2);
+        
+        
         
     }
     
     temp3.moveLeft(x, y);
     if(!checkVisited(temp3)&& !inSucc(temp3) )
     {
-        succ.push(temp3);
+        
+            
+                
+     if(getDistance(temp3, f_state) < current)
+            {
+                current = getDistance(temp3, f_state);
+                succ.push(temp3);
+            }
+            
+        
         
     }
     
     temp4.moveRight(x, y);
     if(!checkVisited(temp4)&& !inSucc(temp4))
     {
-        succ.push(temp4);
+        
+            
+            
+                
+                succ.push(temp4);
+            
+        
         
     }
+    }
+
+    
+    
+   
+    
+    return current;
+
 }
 //-----------------------------------------------------
 //Define a function to check goal state
@@ -301,40 +438,109 @@ bool checkGoal(Board board, Board f_state)
     }
     return true;
 }
+// A utility function to count inversions in given
+// Board. Note that this function can be
+// optimized to work in O(n Log n) time. The idea
+// here is to keep code small and simple.
+int invTCount(Board board)
+{
+    int inv_count = 0;
+    for (int i = 0; i < board.n*board.n - 1; i++)
+    {
+        for (int j = i+1; j < board.n*board.n; j++)
+        {
+            // count pairs(i, j) such that i appears
+            // before j, but i > j.
+            if (board.vec[i/board.n][i%board.n] &&
+                board.vec[j/board.n][j%board.n] &&
+                board.vec[i/board.n][i%board.n] > board.vec[j/board.n][j%board.n])
+                inv_count++;
+        }
+    }
+    return inv_count;
+}
+
+
+//Check whether board solvable
+bool isSolvable(Board board)
+{
+    // Count inversions in given board
+    int invCount = invTCount(board);
+ 
+    // If grid is odd, return true if inversion
+    // count is even.
+    if (board.n & 1)
+        return !(invCount & 1);
+    else     // grid is even
+    {
+        int pos = board.blankX;
+        if (pos & 1)
+            return !(invCount & 1);
+        else
+            return invCount & 1;
+    }
+}
+
+
+
+
 //-----------------------------------------------------
 //Define a function to search the goal state
-void Graph_search(int n)
+void Graph_search(int n, Board board, Board f_state)
 {
- Board board(n);
- board.initialState();
- Board f_state(n);
- f_state.goalState();
- succ.push(board);
+     //check if the given state is solvable
+    if(!isSolvable(board))
+    {
+        cout<<"******The given state is not solvable***********"<<endl;
+        return;
+    }
+    {
+        cout<<"******The given state is solvable***************"<<endl;
+    }
  
+    succ.push(board);
+    int current_distance = getDistance(board, f_state);
+    
+    
+
+    
     while(!succ.empty())
     {
         Board temp = succ.front();
+        
+        
         succ.pop();
+        
+        
         if(checkGoal(temp, f_state))
         {
             cout<<"Goal State Found: "<<endl;
             temp.printState();
+            cout<<endl;
             return;
         }
         else{
-            //   cout<<"Visited State: "<<endl;
-            //   temp.printState();
-             // cout<<endl;
-               generateSuccessors(temp,n);
-              visited.push_back(temp);
+            // cout<<"Visited State: "<<endl;
+            // temp.printState();
+            // cout<<endl;
+            current_distance=generateSuccessors(temp,n, f_state, current_distance);
+
+            // cout<<"current distance: "<<current_distance<<endl;
+            // cout<<"Successors: "<<succ.size()<<endl;
+            
+            visited.push_back(temp);
              
         }
+        
+        
        
     }
 }
 
+
+
 //-------------------------------------------------------------------------------------------
-//=======================================Main Metod==========================================
+//=======================================Main Method==========================================
 //-------------------------------------------------------------------------------------------
 
 
@@ -345,17 +551,24 @@ int main()
     int n;
     cout<<"Enter the board size (3 for 8 puzzel and 4 for 15 puzzel ) : ";
     cin>>n;
+    Board board(n);
+    board.initialState();
+    Board f_state(n);
+    f_state.goalState();
+   
+     
     //call the Graph_search function
-    Graph_search(n);
-
-    // //display the visited states
-    // cout<<"Visited States: "<<endl;
+    Graph_search(n, board, f_state);
+    cout<<"*****************************************************************"<<endl;
+    //display the visited states
+    // cout<<"==================Visited States: =================="<<endl;
     // for(list<Board>::iterator it = visited.begin(); it != visited.end(); it++)
     // {
     //     cout<<"=================="<<endl;
     //     it->printState();
     //     cout<<endl;
     // }
+    // cout<<"Number of visited states: "<<visited.size()<<endl;
 
     return 0;
 }
